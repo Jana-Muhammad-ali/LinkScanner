142fd40073c836459b5354d0bbde3b19ce8e94eb
jana1276 <janamuali1276@gmail.com>
2026-04-16 21:30:43 +0200

    Set application to listen on port 8080

--- checker/Program.cs
+++ checker/Program.cs
@@ -20,6 +20,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 app.Run();
